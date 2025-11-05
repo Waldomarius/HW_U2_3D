cd356b28e8095a77fb181d9fbd6eaf758a27d4f9
Waldomarius <40439764+Waldomarius@users.noreply.github.com>
2025-11-05 22:53:51 +0300

    2025.11.05 Add player on scene and create PlayerController.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,44 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Serializable]
+    public class Boundary
+    {
+        [SerializeField] internal float xMin;
+        [SerializeField] internal float xMax;
+        [SerializeField] internal float yMin;
+        [SerializeField] internal float yMax;
+    }
+    
+    [SerializeField] private float _speed = 1f;
+    [SerializeField] private float _tilt = 10f;
+    
+    [SerializeField] private Boundary _boundary;
+    
+    private Rigidbody _rigidbody;
 
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        
+        Vector3 movement = new Vector3(horizontal, 0f, vertical);
+        _rigidbody.velocity = movement * _speed;
+    
+        // Задаем координаты в доступном интервале.
+        _rigidbody.position = new Vector3(
+            Mathf.Clamp(_rigidbody.position.x, _boundary.xMin, _boundary.xMax),
+            0.0f,
+            Mathf.Clamp(_rigidbody.position.z, _boundary.yMin, _boundary.yMax)
+            );
+        
+        _rigidbody.rotation = Quaternion.Euler(0.0f, 0.0f, _rigidbody.velocity.x * -_tilt);
+    }
 }
